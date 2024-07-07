d7737565ce38d037ee63bae527365f2bf6da1e7e
Le Quang That <145110224+thatlq1812@users.noreply.github.com>
2024-07-07 19:08:25 +0700

    Reset

--- s1_upgradedataset.py
+++ s1_upgradedataset.py
@@ -3,30 +3,30 @@ import os
 import mediapipe as mp
 import csv
 
-class HandDetector:
+class Hand_Detector:
     def __init__(self):
         self.mp_hands = mp.solutions.hands
         self.hands = self.mp_hands.Hands(static_image_mode=True, max_num_hands=2, min_detection_confidence=0.5)
         self.mp_drawing = mp.solutions.drawing_utils
 
     def detect_hands(self, image):
-        # Đọc ảnh
+        # Read image
         if image is None:
             raise ValueError("Cannot read image")
         
-        # Chuyển ảnh sang RGB
+        # Convert image to RGB
         image_rgb = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
 
-        # Phát hiện bàn tay
+        # Process image
         results = self.hands.process(image_rgb)
         boxes = []
-        # Vẽ rectangle bao quanh bàn tay
+        # Draw landmarks on image
         if results.multi_hand_landmarks:
             for hand_landmarks in results.multi_hand_landmarks:
                 x_min, y_min = float('inf'), float('inf')
                 x_max, y_max = float('-inf'), float('-inf')
 
-                # Tìm tọa độ của bounding box
+                # Find bounding box of hand
                 for landmark in hand_landmarks.landmark:
                     x, y = int(landmark.x * image.shape[1]), int(landmark.y * image.shape[0])
                     x_min = min(x_min, x)
@@ -34,7 +34,7 @@ class HandDetector:
                     x_max = max(x_max, x)
                     y_max = max(y_max, y)
                     
-                # Mở rộng bounding box thêm 20 pixels ở tất cả các cạnh
+                # Expand bounding box
                 x_min = int(max(0, x_min - 20))
                 y_min = int(max(0, y_min - 20))
                 x_max = int(min(image.shape[1], x_max + 20))
@@ -67,7 +67,7 @@ class Data_Saver:
 
 # Use the code below to test the HandDetector class
 if __name__ == "__main__":
-    detector = HandDetector()
+    detector = Hand_Detector()
     saver = Data_Saver()
     cap = cv2.VideoCapture(0)
     # Setup camera size
@@ -94,10 +94,12 @@ if __name__ == "__main__":
         ret, frame = cap.read()
         if not ret:
             break
-        frame = cv2.resize(frame, (256, 144))
+        
         cv2.imshow('Frame', frame)
-        box = detector.detect_hands(frame)
+        
         if saving:
+            frame = cv2.resize(frame, (256, 144))
+            box = detector.detect_hands(frame)
             try:
                 saver.save_data(frame, box, image_folder + f'{capture_person}_img_{len(os.listdir(image_folder)) + 1}.jpg', csv_file)
                 print(f'Saved {capture_person}_img_{len(os.listdir(image_folder)) + 1}.jpg')
